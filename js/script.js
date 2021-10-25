d8e33fcfa32bbe21268935ccf4a05ad17904fd2c
Maurizio <proiettomaurizio97@gmail.com>
2021-10-25 14:31:04 +0200

    es complete

--- js/script.js
+++ js/script.js
@@ -4,11 +4,31 @@ per i multipli di 3,
 per i multipli di 5
 e per i valori che sono sia multipli di 3 che di 5. */
 
-
+const contSquare = document.querySelector("#container-square");
 // creo un ciclo con una serie di quadrati con all'interno un numero
+for (let i = 1; i <= 100; i++){
+
+    // metodo 1
+   /*  contSquare.innerHTML += `<div class="square">
+    ${i}
+    </div>` */
+
+    // metodo 2
+    let square = document.createElement("div");
+    square.className = "square";
+    square.append(i);
+    contSquare.append(square);
 
     // se il numero è multiplo di 3 compare la scritta fizz
-
+    if (i % 3 == 0){
+        square.classList.add("fizz")
+    }
     // se il numero è multiplo di 5 compare la scritta buz
-
+    if (i % 5 == 0){
+        square.classList.add("buzz")
+    }
     // se il numero è multiplo sia di 3 che di 5 compare la scritta fizzbuz
+    if (i % 5 === 0 && i % 3 === 0){
+        square.classList.add("fizzbuzz")
+    }
+}
