aa572dbcc8619628eac63959587fd6e5d32064bc
Maurizio <proiettomaurizio97@gmail.com>
2021-10-25 13:15:35 +0200

    logical macrostructure js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* Scrivi un programma che esegua un ciclo da 1 a 100
+e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell’indice
+per i multipli di 3,
+per i multipli di 5
+e per i valori che sono sia multipli di 3 che di 5. */
+
+
+// creo un ciclo con una serie di quadrati con all'interno un numero
+
+    // se il numero è multiplo di 3 compare la scritta fizz
+
+    // se il numero è multiplo di 5 compare la scritta buz
+
+    // se il numero è multiplo sia di 3 che di 5 compare la scritta fizzbuz
